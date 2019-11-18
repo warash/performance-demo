3f384a8e9c568e700994b6975544d8ce854451ac
Michał Majewski <michal.majewski@softwareone.com>
2019-11-18 13:33:56 +0100

    fix

--- src/app/recalculate-view-model-on-fly/recalculate-view-model-on-fly.component.ts
+++ src/app/recalculate-view-model-on-fly/recalculate-view-model-on-fly.component.ts
@@ -1,26 +1,19 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recalculate-view-model-on-fly',
   templateUrl: './recalculate-view-model-on-fly.component.html',
   styleUrls: ['./recalculate-view-model-on-fly.component.scss']
 })
-export class RecalculateViewModelOnFlyComponent implements OnChanges {
+export class RecalculateViewModelOnFlyComponent {
 
   @Input() firstName;
   @Input() lastName;
-
 
   get fullName() {
     console.log('calculate full name');
     return `${this.firstName} ${this.lastName}`;
   }
-
-
-  ngOnChanges() {
-
-  }
-
 
   okClicked() {
   }
